a4bc66232235ed78dab780d55df456899b60e797
Sven Finke <Sven.Finke@sva.de>
2022-06-03 14:31:21 +0200

    Add service definitions to expose apps internally and externally

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,7 @@ cd k8s/dev
 # Deploy invoice-app
 kubectl apply -f deployment_payment-provider.yaml
 kubectl apply -f deployment_invoice-app.yaml
+
+# Deploy services
+kubectl apply -f service_invoice-app.yaml
+kubectl apply -f service_payment-provider.yaml
